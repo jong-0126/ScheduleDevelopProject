ce6cb76adb1ed56019760afe8e8e06a987ef5f2d
Master <baek011226@naver.com>
2025-04-04 14:08:52 +0900

    feat: 댓글 생성 기능 구현

--- src/main/java/com/example/scheduledevelop/controller/CommentController.java
+++ src/main/java/com/example/scheduledevelop/controller/CommentController.java
@@ -3,22 +3,23 @@ package com.example.scheduledevelop.controller;
 import com.example.scheduledevelop.dto.comment.CommentRequestDto;
 import com.example.scheduledevelop.dto.comment.CommentResponseDto;
 import com.example.scheduledevelop.service.CommentService;
-import com.example.scheduledevelop.service.ScheduleService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
 
-    @PostMapping("/{id}")
+    @PostMapping("/{id}/comment")
     public ResponseEntity<CommentResponseDto> create(
             @PathVariable("id") Long scheduleId,
             @RequestBody CommentRequestDto requestDto,
